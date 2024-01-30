f6c8302164b2fe0a04d93e7c84dfaacb4e83627a
Rynn Lee <20amigo04@gmail.com>
2024-01-30 15:09:22 +0600

    .evrn Extension
    Made it a separate class

---
+++ my-file-extension (.evrn)/EvrnFile.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class EvrnFile {
+    public void createFile(String name){
+        try{
+            File file = new File(name+".evrn");
+            if(file.createNewFile()){
+                System.out.println("A file has been created!: " + file.getName());
+            } else {
+                System.out.println("The file already exist");
+            }
+        } catch (IOException e){
+            System.out.println("An error occurred!");
+        }
+    }
+    public void writeToFile(String filename, String text){
+        try {
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+            FileWriter writer = new FileWriter(filename);
+            for(Character i : text.toCharArray()){
+                numbers.add((int) i*2);
+            }
+            String arrayString = numbers.stream().map(Object::toString).collect(Collectors.joining("."));
+            writer.write(arrayString);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred!");
+        }
+    }
+
+    public void readFile(String filename){
+        File file = new File(filename);
+        try{
+            Scanner scanner = new Scanner(file);
+            if(scanner.hasNextLine()){
+                String nextLine = scanner.nextLine();
+                System.out.println("--------------Decrypted--------------\n"+ decrypt(nextLine));
+            }
+        } catch (IOException e){
+            System.out.println("An error occurred!");
+        }
+    }
+    String decrypt(String text){
+        ArrayList<String> letters = new ArrayList<String>();
+        String[] numbers = text.split("\\.");
+        for(String letter : numbers){
+            int number = Integer.parseInt(letter)/2;
+            String asciiLetter = Character.toString((char) number);
+            letters.add(asciiLetter);
+        }
+        return letters.stream().map(Object::toString).collect(Collectors.joining(""));
+    }
+}
--- my-file-extension (.evrn)/Main.java
+++ my-file-extension (.evrn)/Main.java
@@ -6,60 +6,17 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main{
-    static String placeholder = "I just wanted to test if this would work, and it kinda does!\nIt's so cool. Probably in the future i'll develop this file extension further and will use it in my programs!";
-
     public static void main(String[] args) {
-        createFile("everrynn");
-        writeToFile("everrynn.evrn", placeholder);
-        readFile("everrynn.evrn");
-    }
-    static void createFile(String name){
-        try{
-            File file = new File(name+".evrn");
-            if(file.createNewFile()){
-                System.out.println("A file has been created!: " + file.getName());
-            } else {
-                System.out.println("The file already exist");
-            }
-        } catch (IOException e){
-            System.out.println("An error occurred!");
-        }
-    }
-    static void writeToFile(String filename, String text){
-        try {
-            ArrayList<Integer> numbers = new ArrayList<Integer>();
-            FileWriter writer = new FileWriter(filename);
-            for(Character i : text.toCharArray()){
-                numbers.add((int) i*2);
-            }
-            String arrayString = numbers.stream().map(Object::toString).collect(Collectors.joining("."));
-            writer.write(arrayString);
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred!");
-        }
-    }
+        String placeholder = "Oh! Hi there! I see you're interested in what am i doing!\n" +
+                "Well, I was trying to create my own file extension in this one\n" +
+                "It's not really that effective and takes more space than just plain .txt file\n" +
+                "But that was just an attempt so idc.\n" +
+                "Maybe in the future i'll improve it\n" +
+                "And make it like a real extension with a purpose";
+        EvrnFile evrnFile = new EvrnFile();
 
-    static void readFile(String filename){
-        File file = new File(filename);
-        try{
-            Scanner scanner = new Scanner(file);
-            if(scanner.hasNextLine()){
-                String nextLine = scanner.nextLine();
-                System.out.println("Decrypted: "+ decrypt(nextLine));
-            }
-        } catch (IOException e){
-            System.out.println("An error occurred!");
-        }
-    }
-    static String decrypt(String text){
-        ArrayList<String> letters = new ArrayList<String>();
-        String[] numbers = text.split("\\.");
-        for(String letter : numbers){
-            int number = Integer.parseInt(letter)/2;
-            String asciiLetter = Character.toString((char) number);
-            letters.add(asciiLetter);
-        }
-        return letters.stream().map(Object::toString).collect(Collectors.joining(""));
+        evrnFile.createFile("MyNewFile");
+        evrnFile.writeToFile("MyNewFile.evrn", placeholder);
+        evrnFile.readFile("MyNewFile.evrn");
     }
 }
